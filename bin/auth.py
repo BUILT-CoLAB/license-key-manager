69a67c1e999f96306f7efcb595074feebbf7a5de
up201808549 <up201808549@fe.up.pt>
2022-04-08 01:48:49 +0200

    login but not login

--- bin/auth.py
+++ bin/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, url_for, render_template, request, flash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from .models import User
 from werkzeug.security import check_password_hash
 
@@ -21,32 +21,21 @@ def login_post():
     user = User.query.filter_by(email=email).first()
 
 
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
+    if not user: # or not check_password_hash(user.password, password):
+        if (user.password == password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('auth.login'))
 
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
     
-    # Login process
-    #dataInfo = request.get_json()
-    #print(dataInfo)
-    #return 'Login XD'
-
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    #if not user or not check_password_hash(user.password, password):
-    #    flash('Please check your login details and try again.')
-    #    return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    # login_user(user, remember=remember)
-    # 
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
--- bin/main.py
+++ bin/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 from . import db
 
 main = Blueprint('main', __name__)
@@ -8,5 +9,6 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
