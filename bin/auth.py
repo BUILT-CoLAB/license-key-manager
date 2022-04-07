dae4f7aa749ef38c1c0aa1d9ccf5047286309f40
up201808549 <up201808549@fe.up.pt>
2022-04-07 10:36:07 +0200

    login kinda done

--- bin/auth.py
+++ bin/auth.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, url_for, render_template, request, flash
 from flask_login import login_user
+from .models import User
+from werkzeug.security import check_password_hash
+
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -10,10 +13,25 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    #user = db.get_user(email) 
+    user = User.query.filter_by(email=email).first()
+
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+    
     # Login process
-    dataInfo = request.get_json()
-    print(dataInfo)
-    return 'Login XD'
+    #dataInfo = request.get_json()
+    #print(dataInfo)
+    #return 'Login XD'
 
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
@@ -23,7 +41,7 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     # login_user(user, remember=remember)
-    # return redirect(url_for('main.profile'))
+    # 
 
 @auth.route('/signup')
 def signup():
---
+++ main.py
@@ -0,0 +1,44 @@
+#run hello to check if Flask is working....
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/basic_api/hello_world')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/basic_api/entities', methods=['GET', 'POST'])
+def entities():
+    if request.method == "GET":
+        return {
+            'message': 'This endpoint should return a list of entities',
+            'method': request.method
+        }
+    if request.method == "POST":
+        return {
+            'message': 'This endpoint should create an entity',
+            'method': request.method,
+		'body': request.json
+        }
+
+@app.route('/basic_api/entities/<int:entity_id>', methods=['GET', 'PUT', 'DELETE'])
+def entity(entity_id):
+    if request.method == "GET":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should return the entity {} details'.format(entity_id),
+            'method': request.method
+        }
+    if request.method == "PUT":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should update the entity {}'.format(entity_id),
+            'method': request.method,
+		'body': request.json
+        }
+    if request.method == "DELETE":
+        return {
+            'id': entity_id,
+            'message': 'This endpoint should delete the entity {}'.format(entity_id),
+            'method': request.method
+        }
