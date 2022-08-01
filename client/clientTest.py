0c26221bbd524b7481638f0721cf1613accec5f2
JP Dias <jpdias@live.com.pt>
2022-08-01 12:25:08 +0100

    add new workflow

--- client/clientTest.py
+++ client/clientTest.py
@@ -1,4 +1,4 @@
-from cryptography.hazmat.primitives.asymmetric import rsa,padding
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from uuid import uuid1
@@ -6,20 +6,20 @@ import requests
 import base64
 
 publicKey = """-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVpELyef7dYfnC1cismXIUVI9A
-ZDWaxlmZWh41hx8e9ECsWFj29qnjDEw/z67AvFso5V3a679Sh6D5puebnbo5ledJ
-/TE7MT2di8s42RcRTWMkUJcS6/OBk5PoQLEkoZytS5Dguh45mRFccLp6SkpvVNT9
-G8cLTLKt7jSBwTnpiQIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC2F2bV0KErkLrzcox/uaBR5hXy
+1VJ8urKUfOoLybOelAt2nPfsnz1OKJO5RGrysUgQUwpBhjmTa2wDngB0/5sGFCRT
+MLha5yVdEPN1umJKBdzxO/L/bYitK3SkucmiYiHI/t4JkJLu+4x8/SShARchqzw/
+ttmbB6O8tw8sigqGawIDAQAB
 -----END PUBLIC KEY-----
 """
 
-public_key = serialization.load_pem_public_key( str.encode(publicKey) )
+public_key = serialization.load_pem_public_key(str.encode(publicKey))
 
-api_key = '5ef8f2e0-c659-416c-8e57-adeed9361028'
-serial = 'VKJO6-GBDR7-CV5IG-F5Y7U'
-hwid= 'TestingHWID_N1' # Deterministic UID
+api_key = 'f8e6f453-bd47-4576-a0de-3a06a35a453d'
+serial = 'QT074-6MWAP-5DNQ7-440DZ'
+hwid = 'TestingHWID_N2'  # Deterministic UID
 
-plaintexts = bytes(serial + ':' + hwid[:23],'utf-8')
+plaintexts = bytes(serial + ':' + hwid[:23], 'utf-8')
 
 if(isinstance(public_key, rsa.RSAPublicKey)):
 
@@ -32,12 +32,10 @@ if(isinstance(public_key, rsa.RSAPublicKey)):
         )
     )
     print(plaintexts)
-    final_payload=base64.b64encode(payload).decode('utf-8')
-    
-    r = requests.post('http://127.0.0.1:5000/validate',json={
-        "apiKey":api_key,"payload":final_payload
+    final_payload = base64.b64encode(payload).decode('utf-8')
+
+    r = requests.post('http://127.0.0.1:5000/validate', json={
+        "apiKey": api_key, "payload": final_payload
     })
 
     print(f"Response: {r.json()}")
-
-
